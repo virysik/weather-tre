4b9892d60dfb462d87003dc5c7118f87d2a92382
virysik <virysik@gmail.com>
2020-09-28 13:47:26 +0200

    Added css to Weather

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       <Search />
       <br />
       <Weather />
+      <br />
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div>
-      <div className="row City">
-        <div className="col-7 Name">
+    <div className="Weather">
+      <div className="row">
+        <div className="col-7">
           <h1>New York</h1>
         </div>
         <div className="col-4 Data">
@@ -15,16 +16,16 @@ export default function Weather() {
           </ul>
         </div>
       </div>
-      <p className="Description">Sunny</p>;
+      <p>Sunny</p>;
       <div className="row">
         <div className="clearfix">
-          <div className="col-auto float-left mt-3 Icon">
+          <div className="col-auto float-left mt-3">
             <img
               src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
               alt="sunny"
             />
           </div>
-          <div className="col-auto float-left Temp">
+          <div className="col-auto float-left">
             <span className="degrees">18</span>
             <span className="units">
               <a href="/" className="active-link">
@@ -35,11 +36,11 @@ export default function Weather() {
           </div>
         </div>
 
-        <div className="col-5 Atributes">
+        <div className="col-5">
           <br />
           <ul>
-            <li className="humidity">humidity: 80 %</li>
-            <li className="humidity">wind: 10 km/h</li>
+            <li>humidity: 80 %</li>
+            <li>wind: 10 km/h</li>
           </ul>
         </div>
       </div>
