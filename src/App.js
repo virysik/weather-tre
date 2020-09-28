2ebce881962941bd8ef0fdf12f35e2997d63dfbe
virysik <virysik@gmail.com>
2020-09-28 12:24:22 +0200

    deleted unused folders

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import React from "react";
 import "./App.css";
 import Enter from "./Enter";
-import Search from "./Search";
-import City from "./City";
-import Description from "./Description";
-import Weather from "./Weather";
-import Forecast from "./Forecast";
-import Signature from "./Signature";
+
 
 function App() {
   return (
     <div className="App">
-      
         <Enter />
-        <Search />
-        <br />
-        <City />
-        <Description />
-        <Weather />
-        <br />
-        <Forecast />
-        <Signature />
-     
     </div>
   );
 }
